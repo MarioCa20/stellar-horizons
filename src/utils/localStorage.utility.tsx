66158411994e95ee2ba473b0121ac128aa56a19f
Mario <mcanas@unal.edu.co>
2025-07-20 11:32:57 -0500

    Delete old localStorage methods

--- src/utils/localStorage.utility.tsx
+++
@@ -1,7 +0,0 @@
-export const persistLocalStorage = <T,>(key: string, value: T): void => {
-  localStorage.setItem(key, JSON.stringify(value));
-};
-
-export const clearLocalStorage = (key: string): void => {
-  localStorage.removeItem(key);
-};
